37e53af8c73f1dba361862d5872565a702ba1378
n1200450 <caho1200450@gn.iwasaki.ac.jp>
2021-10-07 21:55:09 +0900

    2021/10/07/21/52

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            for (int i = 0; i < args.Length; i++)
+            {
+                string blank = "";
+
+                StreamReader reader = null;
+                try
+                {
+                    reader = new StreamReader($"txtfiles/{args[i]}.txt");
+                    string text = reader.ReadToEnd();
+                    string[] line = text.Replace("\r\n", "\n").Split(new[] { '\n', '\r' });
+                    for (int bp = 0; bp < 4 - args[i].Length; bp++) { blank += "　"; }
+                    Console.Write($"{args[i]}{blank}：");
+                    for (int j = 0; j < line.Length; j++)
+                    {
+                        Console.Write($"{line[j]}　");
+                        if (j == line.Length - 1) { Console.Write("\n"); }
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.Write(e.Message + "\n");
+                    return -1;
+                }
+                finally
+                {
+                    if (reader != null)
+                    {
+                        reader.Close();
+                    }
+                }
+            }
+            return 0;
+        }
+    }
+}
